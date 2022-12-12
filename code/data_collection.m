faf11337d806aa3786622973d8c3d9a2942aa893
vvaradan <vvaradan@student.ethz.ch>
2022-12-12 02:56:20 +0100

    Added centralized mechanism data

---
+++ code/data_collection.m
@@ -0,0 +1,11 @@
+var1_vals = 0.01:0.02:1;
+var2_vals = 1:2:100;
+J_vals = zeros(length(var1_vals),length(var2_vals));
+for ind1 = 1:length(var1_vals)
+    for ind2 = 1:length(var2_vals)
+        var1 = var1_vals(ind1);
+        var2 = var2_vals(ind2);
+        congestion_game_welfare;
+        J_vals(ind1,ind2)=sum(sum(J_total()))/(2000);
+    end
+end
